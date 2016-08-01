1cfcf861694f7b3284595c53188c48daaabf68cd
William Jones <williamgdjones@gmail.com>
2016-08-01 20:08:49 +0100

    Attempt to clean up submodules

---
+++ src/models/__init__.py
@@ -0,0 +1,18 @@
+__all__ = ['numpy']
+
+import numpy as np
+
+import numpy as np
+import itertools as it
+import matplotlib.pyplot as plt
+import math
+from scipy import stats
+import pdb
+from sklearn import preprocessing
+import copy
+from unittest import *
+import itertools
+from bidict import bidict
+import sys
+sys.path.append('/Users/fonz/Projects/Notebooks/Fine-mapping/src')
+from models import *
--- src/models/trait_simulation.py
+++ src/models/trait_simulation.py
@@ -1,33 +1,32 @@
 ### Sample genotypes
-
 def simulate_genotype(n,m,geno_dist):
     """
-    Simulate a genotype of n samples and m causal SNPs with specified genotype distribution for (0,1,2).
+    Simulate a genotype of n samples and m causal Snumpy. with specified genotype distribution for (0,1,2).
     """
-    X=np.zeros([n,m])
+    X=numpy.zeros([n,m])
     for i in range(m):
-        X[:,i] = [np.random.choice(a=[0,1,2],p=geno_dist) for x in range(n)]
-    return np.array(X)
+        X[:,i] = [numpy.random.choice(a=[0,1,2],p=geno_dist) for x in range(n)]
+    return numpy.array(X)
 
 ###example
 # X = simulate_genotype(n=10000,m=30,geno_dist=[0.85,0.1,0.05])
 
-def simulate_traits(X,snp_group,eps=0.5):
+def simulate_traits(X,numpy_group,eps=0.5):
     """
-    SNPs in the form e.g. {3: 0.9, 5:0.4, 8:0.5}. Dictionary values are the linear model coefficients (beta values).
+    Snumpy. in the form e.g. {3: 0.9, 5:0.4, 8:0.5}. Dictionary values are the linear model coefficients (beta values).
     eps is the level of unexplained variance. X is the genotype information.
     """
-    beta = np.array(snp_group.values()).T
-    snps = snp_group.keys()
-    eps_vector = np.array(np.random.normal(0,eps,X.shape[0])).T
-    return np.add(np.dot(X[:,snps], beta), eps_vector)
+    beta = numpy.array(numpy.group.values()).T
+    numpy = snumpy.group.keys()
+    eps_vector = numpy.array(numpy.random.normal(0,eps,X.shape[0])).T
+    return numpy.add(numpy.dot(X[:,snumpy.], beta), eps_vector)
 
 # examples
-# y = simulate_traits(X,eps=0.5,snp_group={3: 5, 9: 3})
+# y = simulate_traits(X,eps=0.5,snumpy.group={3: 5, 9: 3})
 
 def build_linear_models(X,y):
     """
-    Build univariate linear models for each SNP column in X against the trait y.
+    Build univariate linear models for each Snumpy.column in X against the trait y.
     """
     return [stats.linregress(X[:,i],y) for i in range(X.shape[1])]
 
@@ -36,7 +35,7 @@ def build_linear_models(X,y):
 
 def calc_effect_sizes(models):
     """
-    Calculate the effect sizes = beta / se(beta) of individual SNPs towards the traits.
+    Calculate the effect sizes = beta / se(beta) of individual Snumpy. towards the traits.
     Takes in a list of linear regression models.
     """
     return [x.slope / x.stderr for x in models]
